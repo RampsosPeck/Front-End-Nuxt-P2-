17842170385764f0724573777b4f1fecce626a68
RampsosPeck <peralta.jorge.uatf@gmail.com>
2020-05-12 19:02:16 -0400

    Finish complet lesson 10 Perfect

---
+++ pages/login.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="container col-md-4 mt-5">
+		<h2>Login</h2>
+		<br>
+	<form @submit.prevent="submit">
+	  <div class="form-group">
+	    <label for="exampleInputEmail1">Email address</label>
+	    <input v-model.trim="form.email" type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email" autofocus>
+	    <small  class="form-text text-danger">Show errors here.</small>
+	  </div>
+	  <div class="form-group">
+	    <label>Password</label>
+	    <input v-model.trim="form.password" type="password" class="form-control"  placeholder="Password">
+	    <small  class="form-text text-danger">Show errors here.</small>
+	  </div>
+	  <button type="submit" class="btn btn-primary">Ingresar</button>
+	</form>
+	<br>
+	<p>No tiene una cuenta? <nuxt-link to="/register">Registrate</nuxt-link> </p>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				form: {
+					email: '',
+					password: ''
+				}
+			}
+		},
+		methods: {
+			async submit(){
+				await this.$auth.loginWith("local", {
+					data: this.form
+				})
+				this.$router.push('/')
+			}
+		}
+	};
+</script>
+
+
---
+++ pages/register.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="container col-md-4 mt-5">
+		<h2>Registrarse</h2>
+		<br>
+	<form>
+	  <div class="form-group">
+	    <label >Nombre completo:</label>
+	    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Ingrese aqui su nombre completo">
+	    <small  class="form-text text-danger">Show errors here.</small>
+	  </div>
+	  <div class="form-group">
+	    <label >Email address</label>
+	    <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Ingrese su correo">
+	    <small  class="form-text text-danger">Show errors here.</small>
+	  </div>
+	  <div class="form-group">
+	    <label  >Password</label>
+	    <input type="password" class="form-control"  placeholder="Password">
+	    <small  class="form-text text-danger">Show errors here.</small>
+	  </div>
+	  <button type="submit" class="btn btn-primary">Guardar</button>
+	</form>
+	<br>
+	<p>Ya tiene una cuenta? <nuxt-link to="/login">Inicar sesión</nuxt-link> </p>
+	</div>
+</template>
