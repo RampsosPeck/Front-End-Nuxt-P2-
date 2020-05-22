1cc83568cedeea3614fa961d87dfa183d5cce1b5
RampsosPeck <peralta.jorge.uatf@gmail.com>
2020-05-22 02:43:17 -0400

    Finish lesson 16 perfect app

--- pages/topics/_id/index.vue
+++ pages/topics/_id/index.vue
@@ -5,10 +5,36 @@
 			<p class="text-muted">{{ topic.created_at }} por {{ topic.user.name }}</p>
 
 			<div v-for="(content, index) in topic.posts" :key="index" class="ml-5 content">
-				{{ content.body }}
+				<p>{{ content.body }}</p>
+
+				<div v-if="authenticated">
+					<div v-if="user.id === content.user.id"  >
+
+						<button @click.prevent="deletePost(content.id)" class="btn btn-outline-danger fa fa-trash  pull-right"> </button>
+
+						<nuxt-link :to="{name:'topics-posts-edit', params:{id: $route.params.id, body: content.id }}" >
+							<button class="btn btn-outline-success fa fa-edit  pull-right "> </button>
+						</nuxt-link>
+
+
+					</div>
+				</div>
+
 				<p class="text-muted">{{ content.created_at }} por {{ content.user.name }}</p>
 
 			</div>
+		</div>
+		<div class="mt-5 ml-5 mb-5" v-if="authenticated">
+			<form @submit.prevent="create">
+				<div class="form-group">
+					<h4>Añadir nuevo post</h4>
+					<textarea class="form-control" v-model="body" rows="5" placeholder="Escriba aqui su nuevo post"></textarea>
+					<small v-if="errors.body" class="form-text text-danger">{{ errors.body[0] }}</small>
+				</div>
+				<button class="btn btn-outline-primary">
+					Agregar
+				</button>
+			</form>
 		</div>
 	</div>
 </template>
@@ -17,7 +43,8 @@
 	export default {
 		data(){
 			return {
-				topic: ''
+				topic: '',
+				body: ''
 			}
 		},
 		async asyncData({$axios, params}) {
@@ -25,6 +52,27 @@
 			return {
 				topic: data
 			}
+		},
+		methods: {
+			async create(){
+				await this.$axios.$post(`/topics/${this.$route.params.id}/posts`, {body: this.body})
+				this.$router.push('/topics')
+			},
+			async deletePost(id) {
+				await this.$axios.$delete(`/topics/${this.$route.params.id}/posts/${id}`)
+				this.$router.push('/')
+			}
 		}
 	};
 </script>
+
+<style scoped>
+	.content{
+		border-left: 10px solid #e9ebee;
+		padding:  0 10px 0 10px;
+	}
+	.btn-outline-success, .btn-outline-danger {
+		border: none;
+	}
+</style>
+
--- pages/topics/index.vue
+++ pages/topics/index.vue
@@ -3,6 +3,19 @@
 		<h2>Lista de temas</h2>
 		<div v-for="(topic, index) in topics" :key="index"  class="bg-light mt-5 mb-5" style="padding:20px;">
 			<h2> <nuxt-link :to="{name:'topics-id', params:{id:topic.id  }}">{{ topic.title }}</nuxt-link> </h2>
+
+			<div v-if="authenticated">
+				<div v-if="user.id === topic.user.id"  >
+					<nuxt-link :to="{name:'topics-edit', params:{id:topic.id  }}" >
+						<button class="btn btn-outline-success fa fa-edit fa-2x pull-right "> </button>
+					</nuxt-link>
+
+					<button @click.prevent="deleteTopic(topic.id)" class="btn btn-outline-danger fa fa-trash fa-2x pull-right">
+
+					</button>
+				</div>
+			</div>
+
 			<p class="text-muted">{{ topic.created_at }} por {{ topic.user.name }}</p>
 
 			<div v-for="(content, index) in topic.posts" :key="index" class="ml-5 content">
@@ -42,6 +55,10 @@
 				//console.log(key)
 				let {data} = await this.$axios.$get(key)
 				return this.topics = {...this.topics, ...data}
+			},
+			async deleteTopic(id){
+				await this.$axios.$delete(`/topics/${id}`)
+				this.$router.push('/')
 			}
 		}
 	};
@@ -51,5 +68,8 @@
 	.content{
 		border-left: 10px solid #e9ebee;
 		padding:  0 10px 0 10px;
+	}
+	.btn-outline-success, .btn-outline-danger {
+		border: none;
 	}
 </style>
